62bd47816abc2d4f4f2073bcc3dbdc7329da5479
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-02-02 19:53:57 +0900

    20230202

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { AppDataSource } from "./data-source";
+import { Photo } from "./entity/Photo";
 
 AppDataSource.initialize()
-  .then(() => {})
+  .then(() => {
+    console.log("start...");
+
+    const photo = new Photo();
+
+    photo.name = "my photo";
+    photo.description = "my description";
+    photo.filename = "photo.jpg";
+    photo.views = 1;
+    photo.isPublished = true;
+
+    AppDataSource.manager.save(photo);
+  })
   .catch((error) => {
     console.log(error);
   });
