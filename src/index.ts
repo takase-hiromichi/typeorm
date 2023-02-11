f248643baf32fa25531f2ecef21b825a3f65035f
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-02-11 10:06:00 +0900

    20230211

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { AppDataSource } from "./data-source";
 import { Photo } from "./entity/Photo";
+import { PhotoMetadata } from "./entity/PhotoMetadata";
 
 AppDataSource.initialize()
   .then(async () => {
     console.log("start...");
+
+    const photo = new Photo();
+    photo.name = "my new photo...";
+    photo.description = "my description...";
+    photo.filename = "my photo name...";
+    photo.views = 0;
+    photo.isPublished = true;
+
+    const photoMetadata = new PhotoMetadata();
+    photoMetadata.width = 555;
+    photoMetadata.height = 777;
+    photoMetadata.compressed = true;
+    photoMetadata.comment = "my comment...";
+    photoMetadata.oriientation = "portrait";
+    photoMetadata.photo = photo;
+
+    const photoRepository = AppDataSource.getRepository(Photo);
+    const photoMetadataRepository = AppDataSource.getRepository(PhotoMetadata);
+
+    await photoRepository.save(photo);
+    await photoMetadataRepository.save(photoMetadata);
   })
   .catch((error) => {
     console.log(error);
