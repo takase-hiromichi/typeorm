1e2911b62f4a416c27d1b5ad24b1c7df43ee165b
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-02-15 20:41:40 +0900

    20230215

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,22 @@
 import { AppDataSource } from "./data-source";
 import { Photo } from "./entity/Photo";
-import { PhotoMetadata } from "./entity/PhotoMetadata";
 
 AppDataSource.initialize()
   .then(async () => {
     console.log("start...");
 
-    const photo = new Photo();
-    photo.name = "my new photo...";
-    photo.description = "my description...";
-    photo.filename = "my photo name...";
-    photo.views = 0;
-    photo.isPublished = true;
-
-    const photoMetadata = new PhotoMetadata();
-    photoMetadata.width = 555;
-    photoMetadata.height = 777;
-    photoMetadata.compressed = true;
-    photoMetadata.comment = "my comment...";
-    photoMetadata.oriientation = "portrait";
-    photoMetadata.photo = photo;
-
     const photoRepository = AppDataSource.getRepository(Photo);
-    const photoMetadataRepository = AppDataSource.getRepository(PhotoMetadata);
-
-    await photoRepository.save(photo);
-    await photoMetadataRepository.save(photoMetadata);
+    const photos = await photoRepository.find({
+      where: [
+        {
+          id: 6,
+        },
+        {
+          id: 7,
+        },
+      ],
+    });
+    console.log("photos", photos);
   })
   .catch((error) => {
     console.log(error);
